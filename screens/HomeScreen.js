990d5b11bc5d8755b509adc890f327c021bc662b
TheMinmusDerp <71770907+TheMinmusDerp@users.noreply.github.com>
2021-04-20 10:41:58 -0500

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,140 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      chunks: [],
+    };
+  }
+  getWord=(word)=> {
+    var searchKeyword=word.toLowerCase();
+    var url="https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
+    return fetch(url)
+    .then((data)=>{
+        if(data.status===200)
+        {
+            return data.json()
+        }
+        else
+        {
+            return null
+        }
+    })
+    .then((response)=>{
+        var responseObject=response;
+        if(responseObject){
+            var wordData= responseObject.definitions[0]
+            var definition=wordData.description
+            var lexicalCategory=wordData.wordtype
+            this.state({
+                "word" : this.state.text,
+                "definition" : definition,
+                "lexicalCategory":lexicalCategory
+            })
+        }
+        else
+        {
+            this.setState({
+                "word": this.state.text,
+                "definition":"Not Found Bro"
+            })
+        }
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={'#897AB9'}
+          centerComponent={{
+            text: 'Dictionary',
+            style: { color: '#fff', fontSize: 20 },
+          }}
+        />
+
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({
+                text: text,
+                isSearchPressed: false,
+                word: "Loading...",
+                lexicalCategory:'',
+                examples: [],
+                definition: ""
+            });
+          }}
+          value={this.state.text}
+        />
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => {
+              this.setState({isSearchPressed:true});
+              this.getWord(this.state.text)
+          }}>
+        </TouchableOpacity>
+        <View style={styles.detailsContainer}>
+            <Text style={styles.detailsTitle}>
+                Word :{" "}
+            </Text>
+            <Text style={{fontSize:18}}>
+                {this.state.lexicalCategory}
+            </Text>
+        </View>
+        <View style={{flexDirection:'row',flexWrap:'wrap'}}>
+            <Text style={styles.detailsTitle}>
+                Type:{" "}
+            </Text>
+            <Text style={{fontSize:18}}>
+                {this.state.definition}
+            </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b8b8b8',
+  },
+  inputBoxContainer: {
+    flex:0.3,
+    alignItems:'center',
+    justifyContent:'center'  
+  },
+  inputBox: {
+    marginTop: 50,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    outline: 'none',
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
